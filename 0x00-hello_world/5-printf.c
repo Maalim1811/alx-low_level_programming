2153b675847b1557f244555c1b46ed68430b3e85
Maalim1811 <rogerskirocho@gmail.com>
2022-09-15 06:09:24 -0700

    Added 5-printf.c and modified README.md

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string literal.
+ *
+ * Description: The function prints out the string literal
+ * It prints the string literal using the function printf
+ * The string literal is : 'with proper grammar, but the outcome is a piece of
+ * art,'
+ * Return: returns int 0 showing success.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -4,4 +4,5 @@ The exercises include:
 1-compiler
 2-assembler
 3-name
-4-puts
+4-puts.c
+5-printf.c
