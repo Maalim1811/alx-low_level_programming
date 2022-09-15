8bd023e3f21ffd9acbcd0db3300928d3776d0a98
Maalim1811 <rogerskirocho@gmail.com>
2022-09-14 23:01:57 -0700

    modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints a string literal.
- *   *
- *    * Description: The function prints out the string literal
- *     *"Programming is like building a multilingual puzzle
- *      * Return: returns int 0 showing success.
- *       */
+ * main - Prints a string literal.
+ *
+ * Description: The function prints out the string literal
+ *"Programming is like building a multilingual puzzle
+ * Return: returns int 0 showing success.
+ */
 int main(void)
 {
 	        puts("\"Programming is like building a multilingual puzzle");
